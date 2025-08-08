c622fe79486e6682ce6fbdde1ff1fc4d64513d6a
Matthias <matthias.luetgemeier@cancom.de>
2025-08-08 10:43:25 +0200

    test

--- super-azubi-pets/src/serverAlt.ts
+++ super-azubi-pets/src/serverAlt.ts
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
  */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
+  
   app.listen(port, (error) => {
     if (error) {
       throw error;
